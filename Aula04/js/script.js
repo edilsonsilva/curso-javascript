c1341fb1bbfe1bdbbda220561e2a9af06a87baa9
Edilson Silva <edilson@yahoo.com.br>
2023-10-02 10:21:09 -0300

    Calculadora finalizada

---
+++ Aula04/js/script.js
@@ -0,0 +1,17 @@
+let txtano = document.getElementById("txtano");
+let btnverificar = document.getElementById("btnverificar");
+let rs = document.getElementById("rs");
+
+btnverificar.onclick = function () {
+  //   if (txtano.value % 4 == 0) {
+  //     rs.innerHTML = "Este ano é bissexto";
+  //   } else {
+  //     rs.innerHTML = "Este ano não é bissexto";
+  //   }
+
+  let texto = "asjdflajsdflçjasdçlkf";
+  rs.innerHTML =
+    txtano.value % 4 == 0 ? "Este ano é bissexto" : "Este ano não é bissexto";
+};
+
+console.log(z);
---
+++ Aula04/js/script3.js
@@ -0,0 +1,6 @@
+const cidades = ["Leme", "Campinas", "Guarulhos"];
+console.log(cidades);
+console.log(cidades[0]);
+//adicionar uma cidades com o comando push
+cidades.push("Rio de Janeiro");
+console.log(cidades[cidades.length - 1]);
